3d7b7679def144f69e87d6f4cb11c730d257a27f
Dilara Fernando <dilarafernando43@gmail.com>
2026-03-26 23:03:51 +0530

    weather updates

--- src/main/java/lk/ijse/plantgrowthtracking/controller/WeatherController.java
+++ src/main/java/lk/ijse/plantgrowthtracking/controller/WeatherController.java
@@ -1,1 +1,26 @@
 package lk.ijse.plantgrowthtracking.controller;
+
+import lk.ijse.plantgrowthtracking.dto.WeatherResponse;
+import lk.ijse.plantgrowthtracking.service.WeatherService;
+import lk.ijse.plantgrowthtracking.util.APIResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/weather")
+public class WeatherController {
+
+    @Autowired
+    private WeatherService weatherService;
+
+    @GetMapping("/current")
+    public ResponseEntity<APIResponse<WeatherResponse>> getCurrent(@RequestParam(value = "city", required = false, defaultValue = "Colombo") String city) {
+        WeatherResponse resp = weatherService.getCurrentWeather(city);
+        return ResponseEntity.ok(APIResponse.success(resp));
+    }
+
+}
